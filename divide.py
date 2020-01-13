ab3df138affec170890de0e229609d2d6e47482c
yohe4274 <o3a2s9i8s5@gmail.com>
2020-01-14 01:18:26 +0900

    derick internship

--- divide.py
+++ divide.py
@@ -6,7 +6,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 print(current_dir)
 
 current_dir = 'data/JapaneseObject/'
-g_colab_full_path='JapaneseObjectDetection/data/JapaneseObject'
+g_colab_full_path='data/JapaneseObject'
 
 # Percentage of images to be used for the test set
 percentage_test = 10;
